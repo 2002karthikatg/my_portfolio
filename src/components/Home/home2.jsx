6a2627f0aa4eff2953ad815013dd45d8c029e820
karthitg13 <karthikatg13@gmail.com>
2025-11-04 08:32:04 +0530

    socialLinks

--- src/components/Home/home2.jsx
+++ src/components/Home/home2.jsx
@@ -87,7 +87,7 @@ function Home2() {
               <ul className="home-about-social-links">
                 <li className="social-icons">
                   <a
-                    href="https://github.com/soumyajit4419"
+                  href="https://github.com/Karthiyayini13"
                     target="_blank"
                     rel="noreferrer"
                     className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
                 </li>
                 <li className="social-icons">
                   <a
-                    href="https://twitter.com/Soumyajit4419"
+                  href="https://github.com/Karthiyayini13"
                     target="_blank"
                     rel="noreferrer"
                     className="icon-colour  home-social-icons"
@@ -107,7 +107,7 @@ function Home2() {
                 </li>
                 <li className="social-icons">
                   <a
-                    href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/karthi-ganesh-08a672207/"
                     target="_blank"
                     rel="noreferrer"
                     className="icon-colour  home-social-icons"
@@ -117,7 +117,7 @@ function Home2() {
                 </li>
                 <li className="social-icons">
                   <a
-                    href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/ihtrak_5143/"
                     target="_blank"
                     rel="noreferrer"
                     className="icon-colour home-social-icons"
